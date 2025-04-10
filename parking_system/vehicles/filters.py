aa946c3704633cdfd793b74da30f5489bb046c22
kaironn2 <jonathas_oliveira17@hotmail.com>
2025-04-10 01:40:10 -0300

    api filters with rql

---
+++ parking_system/vehicles/filters.py
@@ -0,0 +1,13 @@
+from dj_rql.filter_cls import AutoRQLFilterClass
+
+from vehicles.models import Vehicle, VehicleType
+
+
+class VehicleFilterClass(AutoRQLFilterClass):
+
+    MODEL = Vehicle
+
+
+class VehicleTypeFilterClass(AutoRQLFilterClass):
+
+    MODEL = VehicleType
